4adb40820b6fbe65ca8cfc15c587bcebc8aec3e4
EnilceKarina <enilcecastrov@gmail.com>
2023-10-13 18:54:36 -0300

    segundo Proyecto de logeo a pagina qanova

--- src/test/java/testSuite/Prueba.java
+++ src/test/java/testSuite/Prueba.java
@@ -1,17 +1,18 @@
 package testSuite;
 
-
 import com.google.common.annotations.VisibleForTesting;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import testClass.BusquedaAnimalesGoogle;
+import testClass.Logeo;
+
+import java.text.ParseException;
 
 public class Prueba {
     ChromeDriver webDriver;
-    String url = "https://google.cl";
-
+    String url = "http://www.qanovagroup.com/piloto";
     @BeforeTest
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","driverNavegador/chromedriver");
@@ -28,4 +29,10 @@ public class Prueba {
         BusquedaAnimalesGoogle busquedaAnimalesGoogle = new BusquedaAnimalesGoogle();
         busquedaAnimalesGoogle.busquedaPerro(webDriver);
     }
+    @Test
+    public void pruebaLogin() throws ParseException {
+        Logeo logeo = new Logeo(webDriver);
+        logeo.CasoLogin1("nvivas","qanova");
+    }
+
 }
